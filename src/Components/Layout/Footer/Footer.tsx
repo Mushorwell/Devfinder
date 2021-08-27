aeefc0e2414b0037dd95b93909cd9a508fa02d83
Mushorwell <mushorwell@gmail.com>
2021-08-27 02:57:35 +0200

    View for activity

--- src/Components/Layout/Footer/Footer.tsx
+++ src/Components/Layout/Footer/Footer.tsx
@@ -6,10 +6,10 @@ const Footer = () => {
     return(
         <div className={styles.footer}>
             <div className={styles.extraFooterSection}>
-                <a href="#"><AiFillFacebook color={'#282c34'} size={'40px'}/></a>
-                <a href="#"><AiFillGithub color={'#282c34'} size={'40px'}/></a>
-                <a href="#"><AiFillInstagram color={'#282c34'} size={'40px'}/></a>
-                <a href="#"><AiFillLinkedin color={'#282c34'} size={'40px'}/></a>
+                <a href="https://www.facebook.com/"><AiFillFacebook color={'#282c34'} size={'40px'}/></a>
+                <a href="https://github.com/Mushorwell"><AiFillGithub color={'#282c34'} size={'40px'}/></a>
+                <a href="https://www.instagram.com/"><AiFillInstagram color={'#282c34'} size={'40px'}/></a>
+                <a href="https://www.linkedin.com/in/orwell-tendaishe-mushaikwa-bb30647a/"><AiFillLinkedin color={'#282c34'} size={'40px'}/></a>
             </div>
             <div className={styles.copyright}>
                 Copyright © Orwell Tendaishe Mushaikwa
